48d62585153ed7e8f324b2f7588836f86e02969f
Anshi Patidar <anshi.patidar07@gmail.com>
2026-04-09 00:03:00 +0530

    Adding cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,40 @@
 
 
+// const express = require("express")
+// const mongoose = require("mongoose")
+// const cors = require("cors")
+
+// const app = express()
+// const PORT = process.env.PORT || 5000
+
+
+// app.use(cors({
+//   origin: "https://anshipatidar07.github.io",
+//   methods: ["GET", "POST", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type"],
+//   credentials: false
+// }))
+
+// // VERY IMPORTANT (handles preflight)
+// app.options("*", cors())
+// app.use(express.json())
+
+// // MongoDB connection
+// mongoose.connect(
+//   "mongodb+srv://admin:Shop12345@shop-tracker.v3trtqm.mongodb.net/?appName=shop-tracker"
+// );
+
+// mongoose.connection.once("open", () => {
+//     console.log("MongoDB connected")
+// })
+
+// // Routes
+// const recordRoutes = require("./routes/records")
+// app.use("/records", recordRoutes)
+
+// app.listen(PORT, () => {
+//     console.log("Server running on port " + PORT)
+// })
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -7,23 +42,32 @@ const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors({
+  origin: "https://anshipatidar07.github.io",
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}))
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://anshipatidar07.github.io");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json())
 
-// MongoDB connection
 mongoose.connect(
-  "mongodb+srv://admin:Shop12345@shop-tracker.v3trtqm.mongodb.net/?appName=shop-tracker"
+  "mongodb+srv://admin:Shop12345@shop-tracker.v3trtqm.mongodb.net/shop-tracker?retryWrites=true&w=majority"
 );
 
 mongoose.connection.once("open", () => {
-    console.log("MongoDB connected")
+  console.log("MongoDB connected")
 })
 
-// Routes
 const recordRoutes = require("./routes/records")
 app.use("/records", recordRoutes)
 
 app.listen(PORT, () => {
-    console.log("Server running on port " + PORT)
+  console.log("Server running on port " + PORT)
 })
