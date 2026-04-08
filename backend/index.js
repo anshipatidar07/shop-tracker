df40c0c3532be96034c91156fb674af1ed233a58
Anshi Patidar <anshi.patidar07@gmail.com>
2026-04-09 00:10:10 +0530

    Adding cors

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,4 @@
 
-
-// const express = require("express")
-// const mongoose = require("mongoose")
-// const cors = require("cors")
-
-// const app = express()
-// const PORT = process.env.PORT || 5000
-
-
-// app.use(cors({
-//   origin: "https://anshipatidar07.github.io",
-//   methods: ["GET", "POST", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type"],
-//   credentials: false
-// }))
-
-// // VERY IMPORTANT (handles preflight)
-// app.options("*", cors())
-// app.use(express.json())
-
-// // MongoDB connection
-// mongoose.connect(
-//   "mongodb+srv://admin:Shop12345@shop-tracker.v3trtqm.mongodb.net/?appName=shop-tracker"
-// );
-
-// mongoose.connection.once("open", () => {
-//     console.log("MongoDB connected")
-// })
-
-// // Routes
-// const recordRoutes = require("./routes/records")
-// app.use("/records", recordRoutes)
-
-// app.listen(PORT, () => {
-//     console.log("Server running on port " + PORT)
-// })
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
